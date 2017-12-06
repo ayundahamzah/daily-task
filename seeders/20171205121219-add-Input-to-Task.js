6ffe02571c97749cc674101db997c1b4cf9441b4
Ayunda <ayundahamzah@gmail.com>
2017-12-06 09:40:31 +0700

    createBy-column-to-UserTask

---
+++ seeders/20171205121219-add-Input-to-Task.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    
+      return queryInterface.bulkInsert('Tasks', [{
+        Task: 'Cuci Mobil',
+        price: 30000,
+        estimatedTimeOfWork:30,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Cuci Motor',
+        price: 25000,
+        estimatedTimeOfWork:25,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Wax Mobil',
+        price: 50000,
+        estimatedTimeOfWork:60,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Wax Motor',
+        price: 35000,
+        estimatedTimeOfWork:45,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Cuci Mesin Detail',
+        price: 60000,
+        estimatedTimeOfWork:75,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      }]);
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+  
+      return queryInterface.bulkDelete('Tasks', [{
+        Task: 'Cuci Mobil',
+        price: 30000,
+        estimatedTimeOfWork:30,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Cuci Motor',
+        price: 25000,
+        estimatedTimeOfWork:25,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Wax Mobil',
+        price: 50000,
+        estimatedTimeOfWork:60,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Wax Motor',
+        price: 35000,
+        estimatedTimeOfWork:45,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },{
+        Task: 'Cuci Mesin Detail',
+        price: 60000,
+        estimatedTimeOfWork:75,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      }]);
+  }
+    
+};
