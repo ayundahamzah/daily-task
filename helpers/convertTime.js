b83b2178fa09fe63e8b4f86f8c0731bad8648e22
yulianhp <kono.utsukushi24@gmail.com>
2017-12-08 09:11:20 +0700

    sebelum heroku

--- helpers/convertTime.js
+++ helpers/convertTime.js
@@ -1,12 +1,13 @@
 function convertTime(startPoint,endPoint){
-  let start =startPoint.getTime()
-  let end =endPoint.getTime()
+  let start = startPoint.getTime()
+  let end = endPoint.getTime()
   let result= end - start
 
  let realTime= new Date(result).getMinutes()
 
  
- return realTime  
+ return realTime
 
 }
 
+module.exports = convertTime();
